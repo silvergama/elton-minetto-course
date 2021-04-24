0b9ebadae365486cb469335fe7e6df5cf3db90a8
Silver Gama <silver.mdg@gmail.com>
2021-04-24 02:31:46 -0300

    fix typo

--- core/beer/entity.go
+++ core/beer/entity.go
@@ -5,7 +5,7 @@ type Beer struct {
 	ID    int64     `json:"id"`
 	Name  string    `json:"name"`
 	Type  BeerType  `json:"type"`
-	Style BeerStyle `json:"stype"`
+	Style BeerStyle `json:"style"`
 }
 
 type BeerType int
